5b7388b5e988c5e00797737c414a9bf030f71514
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-18 02:23:29 +0530

    ADDED to create contact person

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,39 @@
-public class AddressBook
+package assignment;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+
+public class AddressBookManager 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException, IOException,JsonMappingException,JsonParseException
 	{
-		System.out.println("Welcome to Address Book Program");
+		ImplAddressBookUtil util = new ImplAddressBookUtil();
+		util.readJson();
+
+		Scanner scanner = new Scanner(System.in);
+
+		boolean isExitAddressBook = false;
+		System.out.println("Address book manager\n");
+
+		while (!isExitAddressBook)
+		{
+			System.out.println("Select menu");
+			System.out.println("1. New Address Book\n ");
+
+			int choice = scanner.nextInt();
+
+			switch (choice)
+			{
+			case 1: util.createNewAddressBook();
+					break;
+
+			default: System.out.println("Invalid option");
+					 break;
+			}
+		}
+		scanner.close();
 	}
 }
